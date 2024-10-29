120a41c6de425a62aaaa4d2ced7cbce68064bc6d
AntiDim19728 <antidim19728@163.com>
2024-10-29 12:58:29 +0800

    Add files via upload

--- src/main/java/io/github/breaklibteam/BreakInfinity.java
+++ src/main/java/io/github/breaklibteam/BreakInfinity.java
@@ -6,12 +6,13 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
+import io.github.breaklibteam.Decimal.Decimal;
 
 @Mod(modid = BreakInfinity.MODID, name = BreakInfinity.NAME, version = BreakInfinity.VERSION)
 public class BreakInfinity {
     public static final String MODID = "BreakInfinity";
     public static final String NAME = "Break Infinity";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.1";
 
     private static Logger logger;
 
